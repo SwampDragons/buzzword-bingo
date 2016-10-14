8e7371a9543fab688c625a581ce85cea8ca5acbc
SwampDragons <megan@saucelabs.com>
2016-10-14 13:16:51 -0700

    add validation script to determine whether a user has gotten a bingo

--- hello.py
+++ hello.py
@@ -4,6 +4,49 @@ import random
 
 app = Flask(__name__)
 
+
+def validate(clicked_list):
+    """Determine whether clicked_list contains any bingos."""
+    # clicked list should be a 25-element list of booleans.  E.g.
+    # [True, False, True, True, False ...]
+
+    # Bingo board indexing looks like this when mapped to 2D:
+
+    # 0  1  2  3  4
+    # 5  6  7  8  9
+    # 10 11 12 13 14
+    # 15 16 17 18 19
+    # 20 21 22 23 24
+
+    bingos = [# horizontal bingos
+              range(0, 5),
+              range(5, 10),
+              range(15, 20),
+              range(20, 25),
+              # vertical bingos
+              range(0, 25, 5),
+              range(1, 25, 5),
+              range(2, 25, 5),
+              range(3, 25, 5),
+              range(4, 25, 5),
+              range(5, 30, 5),
+              # diagonal bingos
+              range(0, 25, 6),
+              range(4, 24, 4)]
+
+    index = 0
+    clicked_indices = []
+    for status in clicked_list:
+        if status is True:
+            clicked_indices.append(index)
+        index += 1
+
+    for bingo in bingos:
+        bingoed = all(x in bingo for x in clicked_indices)
+        if bingoed:
+            return bingo
+
+
 @app.route("/")
 def hello():
     random.seed()
