2897a5655a9d82b0a71d82e69613ec9ea4003561
Garrett D'Amore <garrett@damore.org>
2016-10-14 12:55:43 -0700

    Tabs->spaces.

--- game.py
+++ game.py
@@ -2,68 +2,68 @@ class Board:
     def __init__(seed, dim, words):
         self.rng = random.Random()
         self.seed = seed
-	self.rng.seed(seed)
-	self.dim = dim
-	for row in range (0, dim):
+        self.rng.seed(seed)
+        self.dim = dim
+        for row in range (0, dim):
             for col in range (0, dim):
-		self.words[row][col] = self.rng.choice(words)
-		self.claims[row][col] = false
+                self.words[row][col] = self.rng.choice(words)
+                self.claims[row][col] = false
 
     def session_id():
         return self.seed
 
     def claim(word):
-	""" Claims a word for a given session.  Returns true if the word
-	    is on the board, false otherwise.
-	"""
-	for row in range (0, self.dim):
-	    for col in range (0, self.dim):
-		if self.words[row][col] == word:
-		    claims[row][col] = true
-		    return true
-	return false
+        """ Claims a word for a given session.  Returns true if the word
+            is on the board, false otherwise.
+        """
+        for row in range (0, self.dim):
+            for col in range (0, self.dim):
+                if self.words[row][col] == word:
+                    claims[row][col] = true
+                    return true
+        return false
 
     def winner():
-	""" Returns true if the board is a winner, false otherwise.
-	"""
-	# Check rows, then columns, then diagonals.
-	# Rows first:
-	for row in range (0, self.dim):
+        """ Returns true if the board is a winner, false otherwise.
+        """
+        # Check rows, then columns, then diagonals.
+        # Rows first:
+        for row in range (0, self.dim):
             winner = true
-	    for col in range (0, self.dim):
-	        if not self.claims[row][col]:
-		    winner = false
-		    break
-	    if winner:
-		return true
-	# Columns next
-	for col in range (0, self.dim):
-	    winner = false
-	    for row in range (0, self.dim):
-		if not self.claims[row][col]:
-		    winner = false
-		    break
-	    if winner:
-		return true
-	# Diagonal \ next
-	winner = true
-	for x in range (0, self.dim):
-	    if not self.claims[x][x]:
-		winner = false
-		break
-	if winner:
-		return true
+            for col in range (0, self.dim):
+                if not self.claims[row][col]:
+                    winner = false
+                    break
+            if winner:
+                return true
+        # Columns next
+        for col in range (0, self.dim):
+            winner = false
+            for row in range (0, self.dim):
+                if not self.claims[row][col]:
+                    winner = false
+                    break
+            if winner:
+                return true
+        # Diagonal \ next
+        winner = true
+        for x in range (0, self.dim):
+            if not self.claims[x][x]:
+                winner = false
+                break
+        if winner:
+                return true
 
-	# Diagonal / next
-	winner = true
-	for x in range (0, self,dim):
-	    if not self.claims[x][self.dim-x-1]:
-		winner = false
-		break
-	if winner:
-		return true
+        # Diagonal / next
+        winner = true
+        for x in range (0, self,dim):
+            if not self.claims[x][self.dim-x-1]:
+                winner = false
+                break
+        if winner:
+                return true
 
-	reutrn false
+        reutrn false
 
     # word list is array in self.words
 
@@ -76,18 +76,18 @@ class Game:
     def __init__(self):
         self.rng = random.Random()
         # maybe we don't need all this?
-	self.seed = self.rng.getrandbits(32)
-	self.rng.seed(self.seed)
+        self.seed = self.rng.getrandbits(32)
+        self.rng.seed(self.seed)
         self.words = buzzwords
         self.rows = 5
         self.cols = 5
-	self.boards = {}
+        self.boards = {}
 
     def generate():
 
-	session = self.rng.getrandbits(32)
+        session = self.rng.getrandbits(32)
         board = Board(session, self.rows, self.cols, words)
-	self.boards[session] = board
+        self.boards[session] = board
 
         return board
  
@@ -95,23 +95,23 @@ class Game:
     def claim(session, word):
 
         """ Claims a word for a session.
-	"""
-	board = self.boards[session]
-	if not board:
-	    # invalid session
-	    return false
-	if not board.claim(word):
-	    # word not found on board
-	    return false
+        """
+        board = self.boards[session]
+        if not board:
+            # invalid session
+            return false
+        if not board.claim(word):
+            # word not found on board
+            return false
 
-	return true
-	
+        return true
+        
     def win(session):
         """ Checks if a session is a winner.
-	"""
-	board = self.boards[session]
-	if not board:
-	    # no such session
-	    return false
-	return board.winner()
+        """
+        board = self.boards[session]
+        if not board:
+            # no such session
+            return false
+        return board.winner()
 
